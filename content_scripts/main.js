84c6c485cd706f8c6def6cb39ed5b0f1e765cba0
KilledByNLP <iwasa@katfuji.lab.tuat.ac.jp>
2017-10-28 02:11:16 +0900

    Set rotations invesed when storage['inversed'] returns true

--- content_scripts/main.js
+++ content_scripts/main.js
@@ -2,21 +2,33 @@ var onPageLoad = function() {
   chrome.storage.local.get('rps', function (keyValuePair) {
     // rps shorts for *rotate per second*
     var rps = keyValuePair['rps'];
+
     // set 1 as default value if rotate-speed is not set in localStorage
     if (rps === undefined || rps === null) {
       rps = 1;
       chrome.storage.local.set({'rps': 1}, function () {});
     }
 
-    var style = document.createElement('style');
-    style.innerText += '.ProfileAvatar-image, .js-action-profile-avatar, .avatar, .Avatar, .MomentUserByline-avatar {';
-    style.innerText += '    animation: spin ' + 1.0 / rps + 's linear infinite;';
-    style.innerText += '}';
+    // force formatting "[0-9]+\.[0-9]+"
+    if (parseInt(rps) == rps) {
+      rps = parseInt(s) + '.0';
+    }
 
-    // WebKit hack
-    style.appendChild(document.createTextNode(''));
+    chrome.storage.local.get('inversed', function (keyValuePair) {
+      var inversed = keyValuePair['inversed'];
+      var spin = inversed ? 'spin-inversed' : 'spin';
 
-    document.head.appendChild(style);
+      var style = document.createElement('style');
+
+      style.innerText += '.ProfileAvatar-image, .js-action-profile-avatar, .avatar, .Avatar, .MomentUserByline-avatar {';
+      style.innerText += '    animation: ' + spin + ' ' + 1.0 / rps + 's linear infinite;';
+      style.innerText += '}';
+
+      // WebKit hack
+      style.appendChild(document.createTextNode(''));
+
+      document.head.appendChild(style);
+    });
   });
 };
 onPageLoad();
