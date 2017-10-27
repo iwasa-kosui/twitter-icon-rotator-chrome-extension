357a3cc0eb0efa8a5a03ad9da351bea03585fe35
KilledByNLP <iwasa@katfuji.lab.tuat.ac.jp>
2017-10-28 01:15:26 +0900

    Added .ProfileCardMini-avatarImage

--- content_scripts/main.js
+++ content_scripts/main.js
@@ -1,15 +1,27 @@
+var DEFAULT_RPS = 1;
+var CLASSES = [
+  'avatar',
+  'Avatar',
+  'js-action-profile-avatar',
+  'MomentUserByline-avatar',
+  'ProfileAvatar-image',
+  'ProfileCardMini-avatarImage'
+];
+
 var onPageLoad = function() {
   chrome.storage.local.get('rps', function (keyValuePair) {
     // rps shorts for *rotate per second*
     var rps = keyValuePair['rps'];
     // set 1 as default value if rotate-speed is not set in localStorage
     if (rps === undefined || rps === null) {
-      rps = 1;
-      chrome.storage.local.set({'rps': 1}, function () {});
+      rps = DEFAULT_RPS;
+      chrome.storage.local.set({'rps': DEFAULT_RPS}, function () {});
     }
 
     var style = document.createElement('style');
-    style.innerText += '.ProfileAvatar-image, .js-action-profile-avatar, .avatar, .Avatar, .MomentUserByline-avatar {';
+    style.innerText += '.';
+    style.innerText += CLASSES.join(', .') + ' ';
+    style.innerText += '{';
     style.innerText += '    animation: spin ' + 1.0 / rps + 's linear infinite;';
     style.innerText += '}';
 
